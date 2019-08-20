33f2ac3eb6b923b1e9a66cfb7ef4130cdb0589ec
Zach Owens <zach.owens@leanplum.com>
2019-08-19 23:17:41 -0700

    first

---
+++ Segment Integration Demo/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  Segment Integration Demo
+//
+//  Created by Zach Owens on 6/6/19.
+//  Copyright © 2019 Zach Owens. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var colorLabel: UILabel!
+    
+    
+    @IBOutlet weak var redButton: UIButton!
+    @IBOutlet weak var blueButton: UIButton!
+    @IBOutlet weak var orangeButton: UIButton!
+    @IBOutlet weak var greenButton: UIButton!
+    
+    let textColor = UIColor.white
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        redButton.backgroundColor = UIColor.red
+        blueButton.backgroundColor = UIColor.blue
+        orangeButton.backgroundColor = UIColor.orange
+        greenButton.backgroundColor = UIColor.green
+        
+        redButton.setTitleColor(textColor, for: .normal)
+        blueButton.setTitleColor(textColor, for: .normal)
+        orangeButton.setTitleColor(textColor, for: .normal)
+        greenButton.setTitleColor(textColor, for: .normal)
+        
+        redButton.setTitle("Red Button", for: .normal)
+        blueButton.setTitle("Blue Button", for: .normal)
+        orangeButton.setTitle("Orange Button", for: .normal)
+        greenButton.setTitle("Green Button", for: .normal)
+    }
+
+    @IBAction func sendColorBtn(_ sender: UIButton) {
+        
+        let labelColor = sender.backgroundColor
+        let labelText = sender.currentTitle
+        let labelTextColor = sender.currentTitleColor
+        
+        colorLabel.text = labelText
+        colorLabel.textColor = labelTextColor
+        colorLabel.backgroundColor = labelColor
+    }
+    
+    
+}
+
